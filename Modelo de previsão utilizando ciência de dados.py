00c4f74dffd7f943aef87e3c0ee997602d328b99
Iris Helen <iris.hellen0308@gmail.com>
2023-04-28 20:55:27 -0300

    Inteligencia_artificial

--- Modelo de previsão utilizando ciência de dados.py
+++ Modelo de previsão utilizando ciência de dados.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 tabela = pd.read_csv("barcos_ref.csv")
 print(tabela)
@@ -8,3 +12,33 @@ print(tabela.corr()["Preco"])
 
 sns.heatmap(tabela.corr()[["Preco"]], annot=True, cmap="Blues")
 plt.show()
+
+y = tabela["Preco"]
+x = tabela.drop("Preco", axis=1)
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=1)
+
+modelo_regressaolinear = LinearRegression()
+modelo_arvoredecisao = RandomForestRegressor()
+
+modelo_regressaolinear.fit(x_treino, y_treino)
+modelo_arvoredecisao.fit(x_treino, y_treino)
+
+previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
+previsao_arvoredecisao = modelo_arvoredecisao.predict(x_teste)
+
+print(metrics.r2_score(y_teste, previsao_regressaolinear))
+print(metrics.r2_score(y_teste, previsao_arvoredecisao))
+
+tabela_auxiliar = pd.DataFrame()
+tabela_auxiliar["y_teste"] = y_teste
+tabela_auxiliar["Previsoes ArvoreDecisao"] = previsao_arvoredecisao
+tabela_auxiliar["Previsoes Regressao Linear"] = previsao_regressaolinear
+
+plt.figure(figsize=(15, 6))
+sns.lineplot(data=tabela_auxiliar)
+plt.show()
+
+nova_tabela = pd.read_csv("novos_barcos.csv")
+print(nova_tabela)
+previsao = modelo_arvoredecisao.predict(nova_tabela)
+print(previsao)
